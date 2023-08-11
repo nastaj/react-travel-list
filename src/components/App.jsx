fa46adb3f034d5b1e7f108afee351bcb23289e21
nastaj <G00424689@atu.ie>
2023-08-11 19:03:41 +0200

    refactoring

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(curItem) {
+    setItems((items) => items.filter((item) => item.id !== curItem.id));
+  }
+
+  function handleToggleItem(curItem) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === curItem.id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    confirmed && setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PackingList.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "description":
+      sortedItems = items.toSorted((a, b) =>
+        a.description.localeCompare(b.description)
+      );
+      break;
+    case "packed":
+      sortedItems = items.toSorted(
+        (a, b) => Number(a.packed) - Number(b.packed)
+      );
+      break;
+  }
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
+}
